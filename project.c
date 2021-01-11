de8d7f3c119bb542e5787fe4c58e50839881fef3
Shruti Pandey <33012078+pandey-shruti@users.noreply.github.com>
2021-01-11 23:52:00 +0530

    Add files via upload

--- project.c
+++ project.c
@@ -9,7 +9,6 @@
  */
 
 #include"utils_correct.h"
-#include"krus.h"
 
 int main()
 {
@@ -32,22 +31,35 @@ int main()
 	int* p_index;
 	p_index = malloc(sizeof(int));
 	*p_index = 0;
-	printf("Here are the different route to Manali from Delhi\n");
+	printf("Here are the list of locations that the Travelling Salesman wants to travel in Delhi\n");
 
 
-	struct Edge edges[] =
+	/*struct Edge edges[] =
 	    {
 	        { 0, 3,1 }, { 0, 1,2 }, { 1, 2,2 }, { 2, 3,3 },
-	        { 2, 4, 4 }, { 4, 5,3 }, { 4, 6,1 }, {8,9,5},{ 5, 7,2},{4,8,2},{ 8, 7,5 },
-			{ 6, 9,7 },{ 9, 8,5 }
+	        { 2, 4, 4 }, { 4, 5,3 }, { 4, 6,1 },{ 5, 7,6 },
+			{ 6, 9,7 },{ 7, 8,4 },{ 9, 8,5 },
 
 	    };
-	     int e = sizeof(edges)/sizeof(edges[0]);
+	     int e = sizeof(edges)/sizeof(edges[0]);*/
+
 	     GRAPH * my_graph;
-	     my_graph = make_graph(10, e);
-	    create_adjacency(my_graph, edges,e, citylist);
-	print_graph(my_graph);
-	KruskalMST(my_graph,e,edges);
+	     my_graph = make_graph(10);
+	    create_adjacency(my_graph,0,3,1, citylist);
+		create_adjacency(my_graph,0,1,2, citylist);
+		create_adjacency(my_graph, 1, 2,2, citylist);
+		create_adjacency(my_graph,2, 3,3, citylist);
+		create_adjacency(my_graph,2, 4, 4, citylist);
+		create_adjacency(my_graph,4, 5,3, citylist);
+		create_adjacency(my_graph,4, 6,1, citylist);
+		create_adjacency(my_graph,5, 7,6, citylist);
+		create_adjacency(my_graph,6, 9,7 , citylist);
+		create_adjacency(my_graph,7, 8,4, citylist);
+		create_adjacency(my_graph, 8,4,2, citylist);
+		create_adjacency(my_graph,9, 8,5, citylist);
+	print_graph(my_graph,citylist);
+	printf("\n");
+	prims_array(my_graph);
 	return 0;
 
 }
---
+++ utils_Shruti.h
@@ -0,0 +1,139 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h> 
+
+#define INF 9999999
+
+typedef struct node{
+    char *name;
+    struct node * next;
+    int weight;
+    int value;
+}NODE;
+
+int p_index = 0;
+
+NODE * make_node(int ,char **,int w);
+
+NODE * make_node(int i, char ** l,int w){   // function to make a new node of the graph
+    NODE * s;
+    s = (NODE *) malloc (sizeof(NODE));
+
+    s->name = l[i];
+    s->value = i;
+    s->weight = w;
+    s->next = (NODE *) NULL;
+    return s;
+}
+
+typedef struct GRAPH{
+    int vertices;
+    NODE ** adj;
+}GRAPH;
+
+GRAPH * make_graph(int v){  // function to make a new graph with v number of vertices
+    GRAPH *g;
+    g = (GRAPH *) malloc(sizeof(GRAPH));
+    g -> vertices = v;
+    g -> adj = malloc(v * sizeof(NODE*));
+    for (int i=0; i<v; i++)
+        g->adj[i] = (NODE*) NULL;
+    return g;
+}
+/*struct Edge {
+    int src, dest, cost;
+};*/
+
+void create_adjacency(GRAPH * g, int ps,int ns,int cost,char **l){  // function to add ns in the adjacenecy list of ps
+	//printf(" %s\n", l[s]);
+	/*for (int i = 0; i < e; i++){
+		int s =ed[i].src;
+		int n =ed[i].dest;
+		int cost = ed[i].cost;*/
+
+   NODE * v = make_node(ns, l,cost);
+   v->next = g->adj[ps]; //v pointing to itself
+   //v->weight = cost;
+   g->adj[ps] = v;  //allowed ns added to adj_list of ps
+
+    NODE * u = make_node(ps, l,cost);
+    u->next = g->adj[ns]; //v pointing to itself
+    //u->weight = cost;
+    g->adj[ns] = u;  //allowed ns added to adj_list of ps
+	}
+
+void print_graph(GRAPH * g,char ** l){  // function to print the graph
+    int i;
+    NODE * adj_node;
+    for (i=0 ; i < g->vertices ; i++){
+        adj_node = g->adj[i];
+        //char * source = l[i];
+        printf("\n");
+        printf("adjacency list of vertex %s (vertex no.= %d)\n", l[i],i);
+        while(adj_node){
+            printf("%s (vertex no.= %d, cost = %d)---> ", adj_node->name,adj_node->value,adj_node->weight);
+            adj_node = adj_node->next;
+        }
+        printf("\n");
+    }
+}
+
+
+void prims_array(GRAPH *g) {
+  int edges;  // number of edges in MST
+  edges = 0; //initially no edge in MST
+  int minimum_cost = 0;
+
+  int V = g->vertices;  
+  NODE ** adj = g->adj;
+
+  int setS[20]={false}; // An array to store the whether a vertex is in MST
+  
+  setS[0] = true; // start with the 0th vertex
+
+  int x;  //  source vertex
+  int y;  //  destination vertex
+
+
+  while (edges < V - 1) //MST will always have edges less than or equal to V-1
+  {
+    
+    int min = INF;
+    int x = 0;
+    int y = 0;
+
+    for(int i = 0; i < V; i++)
+    {
+      if (setS[i]) //the vertex in S
+      {
+        NODE * adjacent = adj[i];
+        
+        while(adjacent)
+       {
+          int j = adjacent->value; 
+          if (!setS[j]) // check for the edges across the cut
+          {
+            int price = adjacent->weight;
+            printf("The cost of edge from vertex %d in set S to vertex %d in V-S is %d\n",i,j,price); 
+            if (min > price) {
+              
+              min = price;
+              x = i;
+              y = j;
+            }
+          }
+          adjacent = adjacent->next;
+        }
+      }
+    }
+    
+    printf("The minimum weight edge (light/safe edge) included in MST is %d -> %d having cost of %d\n", x, y, min);
+    minimum_cost += min;
+    printf("\n");
+    setS[y] = true; //include vertex y in S
+    edges++;
+  }
+  printf("The cost of the Minimum Spanning Tree obtained is %d",minimum_cost);
+  return ;
+}
