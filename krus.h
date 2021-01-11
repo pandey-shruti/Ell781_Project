de1df3090b5ae55a77ed3ea6c8075f848638fe7a
MonikaRajan <monikabd766@gmail.com>
2021-01-11 17:46:57 +0530

    Add files via upload

---
+++ krus.h
@@ -0,0 +1,110 @@
+/*
+ * krus.h
+ *
+ *  Created on: 11-Jan-2021
+ *      Author: monik
+ */
+
+#ifndef KRUS_H_
+#define KRUS_H_
+
+
+struct subset {
+    int parent;
+    int rank;
+};
+int find(struct subset subsets[], int i)
+{
+    // find root and make root as parent of i
+    // (path compression)
+    if (subsets[i].parent != i)
+        subsets[i].parent
+            = find(subsets, subsets[i].parent);
+
+    return subsets[i].parent;
+}
+void Union(struct subset subsets[], int x, int y)
+{
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    // Attach smaller rank tree under root of high
+    // rank tree (Union by Rank)
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+
+    // If ranks are same, then make one as root and
+    // increment its rank by one
+    else
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+int myComp(const void* a, const void* b)
+{
+    struct Edge* a1 = (struct Edge*)a;
+    struct Edge* b1 = (struct Edge*)b;
+    return a1->cost > b1->cost;
+}
+void KruskalMST(struct GRAPH* graph, int ed, struct Edge edge[])
+{
+    int V = graph->vertices;
+    struct Edge result[V]; // Tnis will store the resultant MST
+    int e = 0; // An index variable, used for result[]
+    int i = 0; // An index variable, used for sorted edges
+
+    // Step 1:  Sort all the edges in non-decreasing
+    // order of their weight. If we are not allowed to
+    // change the given graph, we can create a copy of
+    // array of edges
+    qsort(graph->adj, ed, sizeof(graph->adj[0]), myComp);
+
+    // Allocate memory for creating V ssubsets
+    struct subset* subsets
+        = (struct subset*)malloc(V * sizeof(struct subset));
+
+    // Create V subsets with single elements
+    for (int v = 0; v < V; ++v) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    // Number of edges to be taken is equal to V-1
+    while (e < V - 1 && i < ed) {
+        // Step 2: Pick the smallest edge. And increment
+        // the index for next iteration
+        struct Edge next_edge = edge[i++];
+
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+
+        // If including this edge does't cause cycle,
+        // include it in result and increment the index
+        // of result for next edge
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+        // Else discard the next_edge
+    }
+
+    // print the contents of result[] to display the
+    // built MST
+    printf(
+        "Following are the edges in the constructed MST\n");
+    int minimumCost = 0;
+    for (i = 0; i < e; ++i)
+    {
+        printf("%d -- %d == %d\n", result[i].src,
+               result[i].dest, result[i].cost);
+        minimumCost += result[i].cost;
+    }
+    printf("Minimum Cost Spanning tree : %d",minimumCost);
+    return;
+}
+
+#endif /* KRUS_H_ */
